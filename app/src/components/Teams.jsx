c832bb58d08a6e033b9bddc68c242b384ee0f20a
unknown <jtpojero@gmail.com>
2019-06-01 13:24:47 -0400

    Added readme and first component

---
+++ app/src/components/Teams.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Teams.css';
+
+class Teams extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+    };
+
+  }
+
+  render(){
+    return (
+      <div className="Teams">
+      </div>
+    );
+  }
+}
+
+export default Teams;
